bbf241b61ea886aa108716ccb4902faae6a0bb25
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2025-09-24 15:02:54 +0300

    improvements

--- OpenClosePrincipleSolution/Invoice.cs
+++ OpenClosePrincipleSolution/Invoice.cs
@@ -13,26 +13,4 @@ namespace OpenClosePrincipleSolution
             return amount - 10;
         }
     }
-
-    public class FinalInvoice : Invoice
-    {
-        public override double GetInvoiceDiscount(double amount)
-        {
-            return base.GetInvoiceDiscount(amount) - 50;
-        }
-    }
-    public class ProposedInvoice : Invoice
-    {
-        public override double GetInvoiceDiscount(double amount)
-        {
-            return base.GetInvoiceDiscount(amount) - 40;
-        }
-    }
-    public class RecurringInvoice : Invoice
-    {
-        public override double GetInvoiceDiscount(double amount)
-        {
-            return base.GetInvoiceDiscount(amount) - 30;
-        }
-    }
 }
---
+++ OpenClosePrincipleSolution/ProposedInvoice.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenClosePrincipleSolution
+{
+    public class ProposedInvoice : Invoice
+    {
+        public override double GetInvoiceDiscount(double amount)
+        {
+            return base.GetInvoiceDiscount(amount) - 40;
+        }
+    }
+}
